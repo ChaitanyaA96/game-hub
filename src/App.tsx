c7ca912eecd7f4bc75f0132c01a7c4788399473a
attardec <chaitanya.attarde46@gmail.com>
2023-10-20 09:41:54 -0400

    Build a colormodeswitch

--- src/App.tsx
+++ src/App.tsx
@@ -12,14 +12,10 @@ function App() {
       <GridItem area="nav">
         <NavBar />
       </GridItem>
-      <Show above="base">
-        <GridItem area="aside" bg="gold">
-          Aside
-        </GridItem>
+      <Show above="lg">
+        <GridItem area="aside">Aside</GridItem>
       </Show>
-      <GridItem area="main" bg="dodgerblue">
-        Main
-      </GridItem>
+      <GridItem area="main">Main</GridItem>
     </Grid>
   );
 }
