861ef179be1a6ec19b7b0b6b6233b3daf6d26f6f
attardec <chaitanya.attarde46@gmail.com>
2023-10-22 20:53:23 -0400

    Remove Key

--- src/services/api-client.ts
+++ src/services/api-client.ts
@@ -3,6 +3,6 @@ import axios from "axios";
 export default axios.create({
     baseURL: "https://api.rawg.io/api",
     params:{
-        key:"1902ba745d7f44b88894e3bb5351181e"
+        key:""
     }
 })
